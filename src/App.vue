01081ae696da79604615a172da2c450a12272cc0
Sambert <sambert@smartshore.nl>
2023-02-06 14:24:01 +0530

    214  Mutations

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,28 @@
   <base-container title="Vuex">
   <the-counter></the-counter>
     <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+
 
 export default {
   components: {
     BaseContainer,
-    TheCounter
+    TheCounter,
+    ChangeCounter
   },
   // data() {
 
   // },
   methods: {
     addOne() {
-      this.$store.state.counter++;
+            this.$store.commit('increment');
     }
   }
 };
