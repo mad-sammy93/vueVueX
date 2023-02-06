1f097ccf0f612e56fc3aecbfe9c64b25a0a0210e
Sambert <sambert@smartshore.nl>
2023-02-06 19:00:09 +0530

    221 Adding More States

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const store =  createStore({
     state() {
         return {
             counter:0,
+            isLoggedIn: false
         };
     },
     mutations: {
@@ -16,7 +17,9 @@ const store =  createStore({
         increase(state, payload) {
             state.counter = state.counter + payload.value;
         },
-
+        setAuth(state,payload){
+            state.isLoggedIn = payload.isAuth
+        }
     },
     actions: {
         increment(context) {
@@ -28,6 +31,12 @@ const store =  createStore({
         increase(context,payload) {
             console.log(context);
             context.commit('increase',payload);    
+        },
+        login(context) {
+            context.commit('setAuth', { isAuth: true });
+        },
+        logout(context) {
+            context.commit('setAuth', { isAuth: false });
         }
     },
     getters: {
@@ -43,6 +52,9 @@ const store =  createStore({
                 return 100;
             }
             return finalCounter;
+        },
+        userIsAuthenticated(state) {
+            return state.isLoggedIn;
         }
     }
 })
