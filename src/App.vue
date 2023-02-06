86be2e7399daf8e5567518282ba9a489ff635bd7
Sambert <sambert@smartshore.nl>
2023-02-06 12:49:30 +0530

    212,213 setup and connecting components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+  <the-counter></the-counter>
+    <button @click="addOne">Add 1</button>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter
   },
+  // data() {
+
+  // },
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    }
+  }
 };
 </script>
 
